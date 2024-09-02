2d3d11b7a0161ccfa97888b69c53e222fc9ea60c
bobchopra <bobchopracoding@gmail.com>
2024-09-02 09:45:59 -0400

    revise console.log,varibles,datatypes,template litterals, and typeof()

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("this line is being printed from an external javascript file that is being linked using the src tag in the index.html file.")
+// global varibles
+let studentName = "Bob";
+let studentAge = 9;
+
+//template litterilles
+console.log(`hello i am ${studentName} and i am ${studentAge} years old`)
+//using a method along with template litterals
+console.log(`the datatype of studentName is:${typeof(studentName)}`)
+console.log(`the datatype of studentAge is:${typeof(studentAge)}...`)
